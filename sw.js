df37afe7c158aaf89c3af9680471cbf8ee23e6d6
fabiojose8759-ui <fabiojose8759@gmail.com>
2026-06-12 17:33:13 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'inforwnet-v1';
+const ASSETS = [
+  '/inforwnet-vendas/',
+  '/inforwnet-vendas/index.html',
+  '/inforwnet-vendas/style.css',
+  '/inforwnet-vendas/app.js',
+  '/inforwnet-vendas/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Não intercepta requisições do Firebase
+  if (e.request.url.includes('firebase') || e.request.url.includes('googleapis')) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
